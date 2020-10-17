2bebf194d8faa896fcc6234b4bcbddab7a741c4b
Antonio Arellano <KD@Antonios-MacBook-Pro.local>
2020-10-16 20:35:11 -0700

    ? and Select corrected

--- PSTestv1.ps1
+++ PSTestv1.ps1
@@ -1,3 +1,3 @@
-Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
+Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName| where-object {$_ -notmatch "_none_"}|select-object -First 1)
 $EXOSession = New-ExoPSSession -UserPrincipalName "usarea01adm@ingrammicro.com"
 Import-PSSession $EXOSession
